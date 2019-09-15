01ae8d6d2121c090f5ea52d3312621b0169206f5
Vykuntarao Nadakuditi <vykuntarao.n@gmail.com>
2019-09-15 14:42:56 -0400

    Update index.js

--- dockercompose/simpleweb-handle-errors/index.js
+++ dockercompose/simpleweb-handle-errors/index.js
@@ -1,1 +1,24 @@
-fsfs
+const express = require('express');
+const redis = require('redis');
+const app = express();
+//Include the Process to force exit
+const process= require('process');
+
+const client = redis.createClient({
+        host:'redis-server',
+        port:6379
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+// Exit the Process
+        process.exit(0);
+        client.get('visits', (err, visits) => {
+                res.send('Number of visits is' + visits);
+                client.set('visits', parseInt(visits) + 1);
+  });
+});
+
+app.listen(8080, () => {
+         console.log('Listening on port 8080');
+});
